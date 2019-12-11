9ee72fb50923dbc428ea07761c1143e8bc3a9ed7
varnittewari <vxt6823@rit.edu>
2019-12-10 20:28:57 -0500

    changed the design

--- services/BudgetManagerAPI/app/setup/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose'),
-      UserModel = require('@BudgetManagerModels/user');
-const models = {
-  User: mongoose.model('User')
-}
-module.exports = models;
--- services/BudgetManagerAPI/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret: 'budgetsecret',
-    session: { session: false },
-    database: 'mongodb://127.0.0.1:27017/budgetmanager'
-}
